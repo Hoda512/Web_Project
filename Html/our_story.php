e8671b3402785fcedbcd999ade8b384da3b5eb0d
nora_ro <n14930070@gmail.com>
2025-11-25 00:45:19 +0200

    initial commit

---
+++ Html/our_story.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-compatible" content="IE=edge">
+    <meta name="viewport">
+    <title>Heritage musuem</title>
+    <link rel="stylesheet" href="../css/story_style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+    <script src="https://kit.fontawesome.com/3f7db2a477.js"
+     crossorigin="anonymous"></script>
+</head>
+<body>
+<header>
+    <input type="checkbox" name="" id="toggler">
+    <label for="toggler" class="fas fa-bars"></label>
+
+      <a href="#" class="logo">Heritage</a>
+    <nav class="nav1">
+          <a href="#">home</a></li>
+          <a href="#">About</a></li>  
+            <a href="#">categories</a></li>
+           <a href="#">contact us</a></li> 
+           <a href="#">exhibits</a></li>  
+                
+             
+
+     </nav>
+</header>
+
+<section class="home">
+    <div class="content">
+        <h3> our story</h3>
+        <span>Preserving History for Future Generations</span>
+
+    </div>
+</section>
+<section class="About" id="About">
+    <h1 class="heading"><span>our</span> mission</h1>
+    <div class="content">
+        <p> to preserve and share the rich tapestry of human history through 
+            our collection,fostering a deeper understanding of our shared
+             past and inspiring the innovators of tomorrow
+        </p>
+        <p>for over a century, the heritage musuem has been a sanctuary for history
+         a place where the past is not just remembered but relived. we are dedicated to the meticious
+        preservation and artful presentation of humanity's most significant 
+        cultural achievements </p>
+    </div>
+    <h1 class="heading2"> a journey through time </h1>
+
+    <div class="content2">
+    <h3>1921-the founding</h3>
+      <p>  established by philanthropist evelyn reed, the musuem opened it's doors
+        with a modest collection of artifacts,driven by a vision to make history accessible to all.
+        </p> 
+
+ <h3>1965-a new wing</h3>
+      <p> the grand atrium was added, an architectural marvel that allowed for displayof larger-scale
+        artifacts, including the now-famous egyption sarcophagi collection.
+        </p> 
+
+ <h3>2023-digital horizon</h3>
+      <p> launching a comprehensive digital archive, we made our collection globally accessible
+        ,embracing technology to further our mission in the 21st century.
+        </p> 
+
+    </div>
+</section>
+
+<section class="collections">
+     <h1 class="heading2"> collections & space </h1>
+     <div class="container">
+      <div class="row">
+        
+        <div class="col-lg-3 col-md-6 col-12">
+            <div class="box">
+            <img src="../img/ima1.jpeg" alt="col1">
+            </div>
+        </div>
+         <div class="col-lg-3 col-md-6 col-12">
+            <div class="box">
+            <img src="../img/im2.jpeg" alt="col1">
+            </div>
+        </div> 
+        <div class="col-lg-3 col-md-6 col-12">
+            <div class="box">
+            <img src="../img/im3.jpeg" alt="col1">
+            </div>
+        </div>
+         <div class="col-lg-3 col-md-6 col-12">
+            <div class="box">
+            <img src="../img/im4.jpeg" alt="col1">
+            </div>
+        </div>
+         <div class="col-lg-3 col-md-6 col-12">
+            <div class="box">
+            <img src="../img/im8.jpeg" alt="col1">
+            </div>
+        </div>
+         <div class="col-lg-3 col-md-6 col-12">
+            <div class="box">
+            <img src="../img/im5.jpeg" alt="col1">
+            </div>
+        </div>
+         <div class="col-lg-3 col-md-6 col-12">
+            <div class="box">
+            <img src="../img/im6.jpeg" alt="col1">
+            </div>
+        </div>
+         <div class="col-lg-3 col-md-6 col-12">
+            <div class="box">
+            <img src="../img/im7.jpeg" alt="col1">
+            </div>
+        </div>
+        
+      </div>
+    </div>
+</section>
+
+<section class="last">
+   <footer>
+    <div>
+   <h4> the Heritage musuem</h4>     
+  <pre>123 history lane, heritage city,10001
+    contact@heritagemuseum.org
+  </pre>
+</div>
+
+   <div id="box"> 
+
+        <h4>quick links</h4>
+        <ul>
+       <li><a href="#">exhibits</a></li> 
+       <li><a href="#">collections</a></li> 
+       <li><a href="#">membership</a></li> 
+       <li><a href="#">press</a></li> 
+        
+        </ul>
+    </div>
+  
+    <div id="icon">
+        <h4>follow us</h4>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-square-facebook"></i>
+    </div>
+
+   
+    
+</footer> 
+<br>
+<div class="rights" align="center"> &copf; 2024 the heritage musuem all rights reserved </div>
+</section>
+
+<script src="bootstrap.bundle.min.js"></script>
+</body>
+</html>
